30ce89fc29dc1ee63da6e49ccf233306ede996d5
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2019-06-27 00:19:16 +0100

    Updates to plot: this/past year colours + group = "none"

--- R/plot.R
+++ R/plot.R
@@ -1,7 +1,7 @@
 #' Interactive semnar maps
 #'
 #' @param x an object of class \code{"semnar"}. See \code{\link{add_presentation}}.
-#' @param group according to what should the semnars be selected on the map? Available options are \code{"year"} (default), \code{"month"}, \code{"presenter"}, \code{"event"}, \code{"country"}, \code{"city"}, \code{"year+country"}.
+#' @param group according to what should the semnars be selected on the map? Available options are \code{"none"} \code{"year"}, \code{"month"}, \code{"presenter"}, \code{"event"}, \code{"country"}, \code{"city"} (default), \code{"year+country"}.
 #' @param title character string for the title of the map. Default is \code{NA}, which produces no title.
 #' @param title_position the position of the title on the map, if \code{title} is not \code{NA}. Available options are \code{"bottomleft"} (default), \code{"bottomright"}, \code{"topleft"}, \code{"topright"}.
 #' @param provider the provider of tiles for the base map. See \code{\link[leaflet]{addProviderTiles}}. Default is \code{"OpenStreetMap.Mapnik"}.
@@ -12,8 +12,18 @@
 #' @param show_event_url Should the event or talk/seminar URL (\code{"link"} argument in \code{\link{add_presentation}}) be printed in the popups (\code{TRUE}), or the event name (\code{"event"} argument in \code{\link{add_presentation}}) become a hyperlink pointing to the event or talk/seminar URL (\code{FALSE}; default).
 #' @param width As in \code{\link[leaflet]{leaflet}}. Default is \code{NULL}.
 #' @param height As in \code{\link[leaflet]{leaflet}}. Default is \code{NULL}.
-#' @param opacity As in \code{\link[leaflet]{markerOptions}}. Default is \code{1}.
+#' @param opacity As in \code{\link[leaflet]{markerOptions}}. Default is \code{0.2}.
+#' @param this_year_colour Colour to be used for this year's presentations. Default is \code{"#737373"}. See Details.
+#' @param past_year_colour Colour to be used for past year's presentations. Default is \code{"#ef3b2c"}. See Details.
 #' @param ... Arguments to be passed to other methods. Currently unused.
+#'
+#'
+#' @details
+#'
+#' A legend with the colour-year combination is printed only if
+#' \code{this_year_colour != past_year_colour}. The current year is
+#' determined by \code{lubridate::year(Sys.time())}.
+#'
 #' @export
 #' @examples
 #' library("magrittr")
@@ -46,7 +56,7 @@
 #'        title = "<a href='https://cran.r-project.org/package=semnar'>semnar</a> map")
 #' }
 plot.semnar <- function(x,
-                        group = "year",
+                        group = "city",
                         title = NA,
                         title_position = "bottomleft",
                         provider = "OpenStreetMap.Mapnik",
@@ -57,7 +67,9 @@ plot.semnar <- function(x,
                         width = NULL,
                         height = NULL,
                         show_event_url = FALSE,
-                        opacity = 1,
+                        opacity = 0.2,
+                        past_year_colour = "#737373",
+                        this_year_colour = "#ef3b2c",
                         ...) {
     object <- x
     if (shorten_url) {
@@ -65,7 +77,7 @@ plot.semnar <- function(x,
     }
     date_format <- match.arg(date_format, choices = c("dmy", "mdy", "ymd", "ydm"))
     group <- match.arg(group,
-                       choices = c("month", "year", "presenter", "event", "country", "city", "year+country"))
+                       choices = c("none", "month", "year", "presenter", "event", "country", "city", "year+country"))
     title_position <- match.arg(title_position,
                                 choices = c("topleft", "bottomleft", "topright", "bottomright"))
 
@@ -104,7 +116,6 @@ plot.semnar <- function(x,
                                       ifelse(is.na(end), "",
                                              strftime(end, format = paste(date_format, "%H:%M"), tz = tz))),
                                 sep = "<br/>"))
-            ## FIX
         }
         ret
     }
@@ -152,6 +163,8 @@ plot.semnar <- function(x,
             )})
     }
 
+    this_year <- year(Sys.time())
+
     ## Base map (provider),
     ## A few options
     ## "Stamen.Toner", "Stamen.TonerLite", "CartoDB.Positron", "Stamen.Watercolor",
@@ -160,33 +173,58 @@ plot.semnar <- function(x,
     p <- leaflet(width = width, height = height) %>%
         addProviderTiles(provider)
 
-    ## Change here
-    object$group <- switch(group,
-                           "month" = as.character(month(object$start, label = TRUE, abbr = FALSE)),
-                           "year" = year(object$start),
-                           "presenter" = object$presenter,
-                           "event" = object$event,
-                           "country" = object$country,
-                           "city" = object$city,
-                           "year+country" = paste(year(object$start), "-", object$country))
+
+    if (!identical(this_year_colour, past_year_colour)) {
+        p <- p %>% addLegend(colors = c(this_year_colour, past_year_colour),
+                             labels = paste(c("=", "<"), this_year),
+                             opacity = opacity,
+                             position = "bottomright")
+    }
 
 
-    object_split <- split(object, object$group)
-    names(object_split) %>% walk(function(group) {
-        p <<- addMarkers(p,
-                         data = object_split[[group]],
-                         lng = ~ lon,
-                         lat = ~ lat,
-                         popup = ~ popup_text,
-                         group = as.character(group),
-                         clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = FALSE),
-                         labelOptions = labelOptions(noHide = TRUE, direction = "auto"),
-                         options = list(opacity = opacity))
-    })
+    years <- year(object$start)
+    object$colour <- ifelse(years < this_year, past_year_colour, this_year_colour)
 
-    p <- addLayersControl(p,
-                          overlayGroups = names(object_split),
-                          options = layersControlOptions(collapsed = TRUE))
+    if (group != "none") {
+        object$group <- switch(group,
+                               "month" = as.character(month(object$start, label = TRUE, abbr = FALSE)),
+                               "year" = year(object$start),
+                               "presenter" = object$presenter,
+                               "event" = object$event,
+                               "country" = object$country,
+                               "city" = object$city,
+                               "year+country" = paste(year(object$start), "-", object$country))
+
+
+        object_split <- split(object, object$group)
+        names(object_split) %>% walk(function(group) {
+            p <<- addCircleMarkers(p,
+                                   data = object_split[[group]],
+                                   lng = ~ lon,
+                                   lat = ~ lat,
+                                   popup = ~ popup_text,
+                                   group = as.character(group),
+                                   color = ~ colour,
+                                   clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = FALSE),
+                                   labelOptions = labelOptions(noHide = TRUE, direction = "auto"),
+                                   fillOpacity = opacity)
+        })
+
+        p <- addLayersControl(p,
+                              overlayGroups = names(object_split),
+                              options = layersControlOptions(collapsed = TRUE))
+
+    }
+    else {
+        p <- addCircleMarkers(p,
+                              data = object,
+                              lng = ~ lon,
+                              lat = ~ lat,
+                              popup = ~ popup_text,
+                              color = ~ colour,
+                              labelOptions = labelOptions(noHide = TRUE, direction = "auto"),
+                              fillOpacity = opacity)
+    }
 
     if (!is.na(title)) {
         map_title <- paste0(title,
